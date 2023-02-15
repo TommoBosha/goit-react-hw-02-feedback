c75ceefdf745f241e09f21b5a78e75baab90e841
TommoBosha <116563699+TommoBosha@users.noreply.github.com>
2023-02-15 23:14:55 +0200

    add solution without styles

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,20 @@
+import { Button } from "./FeedbackOptions.styled";
+import PropTypes from 'prop-types';
+
+export default function FeedbackOptions({ options, onLeaveFeedback }) {
+    return (
+        <>{options.map(option =>  
+    (<Button 
+      key={option}
+            onClick={onLeaveFeedback}
+            >
+       {option}
+      </Button>
+    ))}</>
+    )
+}
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const SectionWrap = styled.section`
+align-items:center;
+display:flex;
+justify-content:center;
+flex-direction:column;`;
+
+export const Title = styled.h1`
+font-size: 36px;
+text-align: center;
+font-weight: 700;
+
+`
+
+
